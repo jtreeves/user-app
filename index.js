4aadec6fcfdcf42607192e3bf28c2053949bdd41
jtreeves <jackson.t.reeves@gmail.com>
2020-11-19 12:11:31 -0500

    [sql] Play with one-to-many functions and queries

--- index.js
+++ index.js
@@ -1,43 +1,89 @@
-// Add all models
-const db = require('./models')
+// // Add all models
+// const db = require('./models')
 
-// Create a user inside users table
-db.user
-    .create({
-        firstName: 'Billy',
-        lastName: 'Bob',
-        age: 40,
-        email: 'notjr@jr.com'
-    }).then(createdUser => {
-        console.log(createdUser.get())
+const db = require("./models");
+
+// // Create a user inside users table
+// db.user
+//     .create({
+//         firstName: 'Billy',
+//         lastName: 'Bob',
+//         age: 40,
+//         email: 'notjr@jr.com'
+//     })
+//     .then(createdUser => {
+//         console.log(createdUser.get())
+//     })
+
+// // Read from users table
+// db.user
+//     .findOne({
+//         where: { firstName: 'Jackson' }
+//     })
+//     .then(foundUser => {
+//         console.log(foundUser.get())
+//     })
+
+// // Find all users
+// db.user.findAll().then(allUsers => { console.log(allUsers) })
+
+// // Update a user
+// db.user.update({
+//     lastName: 'Bobby'
+// }, {
+//     where: { firstName: 'Billy' }
+// }).then(numRowsChanged => {
+//     console.log(numRowsChanged)
+// })
+
+// // Delete a user
+// db.user.destroy({
+//     where: { lastName: 'Bobby' }
+// }).then(numRowsDeleted => {
+//     console.log(numRowsDeleted)
+// })
+
+// db.user.findOne().then((foundUser) => {
+//     foundUser.createPet({
+//         name: 'Violet',
+//         species: 'Retriever'
+//     })
+// })
+
+// db.user.findOne().then((foundUser) => {
+//     foundUser.getPets().then((foundPets) => {
+//         console.log(foundPets)
+//     })
+// })
+
+// db.pet.findOne({
+//     where: {
+//         name: 'Violet'
+//     }
+// }).then((violet) => {
+//     violet.getUser().then((foundUser) => {
+//         console.log(foundUser)
+//     })
+// })
+
+// db.pet.create({
+//     name: 'Hercules',
+//     species: 'Min pin'
+// }).then((herc) => {
+//     console.log(herc)
+// })
+
+db.pet.findOne({
+    where: {
+        name: 'Hercules'
+    }
+}).then((herc) => {
+    // This will result in a race conflict because 83 will run before 82 finishes
+    // const user = db.user.findOne()
+    // herc.addUser(user)
+    db.user.findOne().then((foundUser) => {
+        herc.setUser(foundUser)
     })
-
-// Read from users table
-db.user
-    .findOne({
-        where: { firstName: 'Jackson' }
-    })
-    .then(foundUser => {
-        console.log(foundUser.get())
-    })
-
-// Find all users
-db.user.findAll().then(allUsers => { console.log(allUsers) })
-
-// Update a user
-db.user.update({
-    lastName: 'Bobby'
-}, {
-    where: { firstName: 'Billy' }
-}).then(numRowsChanged => {
-    console.log(numRowsChanged)
 })
 
-// Delete a user
-db.user.destroy({
-    where: { lastName: 'Bobby' }
-}).then(numRowsDeleted => {
-    console.log(numRowsDeleted)
-})
-
-// DON'T NEED THE .THENS IN ORDER TO WORK, IT'S JUST FOR US TO SEE WHAT IT'S DOING
+// // DON'T NEED THE .THENS IN ORDER TO WORK, IT'S JUST FOR US TO SEE WHAT IT'S DOING
